e3c84a4fbff26b18621017518a925f8ac6562a40
unknown <rextype15@gmail.com>
2015-09-14 01:25:57 -0500

    Final Commit

--- src/test/java/mx/iteso/TacitKnowledgePrinterTest.java
+++ src/test/java/mx/iteso/TacitKnowledgePrinterTest.java
@@ -17,33 +17,35 @@ public class TacitKnowledgePrinterTest {
         //What should we verify?
 
     private Printer printer;
-    private TacitKnowledgePrinterTest tkPrinter;
+    private TacitKnowledgePrinterTest tkPrinterTest;
 
     @Before
     public void setUp() {
         printer = mock(Printer.class);
-        tkPrinter = new TacitKnowledgePrinterTest(printer);
+        tkPrinterTest = new TacitKnowledgePrinterTest(printer);
     }
 
     @Test
     public void Verify10Times() {
-        tkPrinter.printNumbers(10);
+        tkPrinterTest.printNumbers(10);
         verify(printer, times(10)).printNumbers(anyString());
     }
 
     @Test
     public void VerifyExpectedResult() {
-
+        tkPrinterTest.calculate(2);
+        verify(printer, "12")
     }
 
     @Test(expected = RuntimeExpection.class)
     public void ExceptionWhenLimit0() {
-        tkPrinter.printNumbers(0);
+        tkPrinterTest.printNumbers(0);
     }
 
     @Test
     public void ExtraTest() {
-
+        tkPrinterTest.isDivisibleby(2,2);
+        verify(printer, true);
     }
 
     @Test
